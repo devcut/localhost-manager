7945bc5fd30ca977fe8345457c6383d768297d03
devcut <guillaume.egloff@gmail.com>
2020-01-16 11:24:39 +0100

    PreferredLanguage fix

--- src/EventListener/ConfigListener.php
+++ src/EventListener/ConfigListener.php
@@ -26,18 +26,7 @@ class ConfigListener implements EventSubscriberInterface
     public function setUserLocale(RequestEvent $event)
     {
         $request = $event->getRequest();
-
-        if (!$request->hasPreviousSession()) {
-            return;
-        }
-
-        // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
-        } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
-        }
+        $request->setLocale($request->getPreferredLanguage());
     }
 
     public function onKernelRequest(RequestEvent $event): void
